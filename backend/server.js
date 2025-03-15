be272ed38a351a42b78a9ea1f33b0ae314d3ef21
Siddhant Pandey <siddhantpandey1305@gmail.com>
2025-03-15 23:34:32 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import mongoose from 'mongoose';
+import postRoutes from "./routes/posts.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+
+app.use(postRoutes);
+
+app.use(express.json());
+
+const start = async () => {
+
+    const connectDB = await mongoose.connect("mongodb+srv://siddhantpandey1305:linkedinclone@linkedin-clone-cluster.5d8uz.mongodb.net/?retryWrites=true&w=majority&appName=linkedin-clone-cluster")
+
+    app.listen(9090, () => {
+        console.log("Server is running on Port:9090")
+    })
+};
+
+start();
