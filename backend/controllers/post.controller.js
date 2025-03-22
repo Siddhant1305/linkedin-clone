3bb02d6bfafaf6308938974b990defc89e457f14
Siddhant Pandey <siddhantpandey1305@gmail.com>
2025-03-22 20:52:17 +0530

    postFunction

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -1,3 +1,4 @@
+import Post from "../models/post.model.js";
 import Profile from "../models/profile.model.js";
 import User from "../models/user.model.js";
 
@@ -7,4 +8,31 @@ export const activeCheck = async (req, res) => {
 
     return res.status(200).json({ message : "RUNNING" })
 
+}
+
+export const createPost = async (req, res) => {
+
+    const { token } = req.body;
+
+    try {
+        const user = await User.findOne({ token: token });
+        
+        if(!user) {
+            return res.status(404).json({ message: "User Not Found" })
+        }
+
+        const post = new Post({
+            userId: user._id,
+            body: req.body.body,
+            media: req.file != undefined ? req.filename : "",
+            fileType: req.file != undefined ? req.file.mimetype.split("/") : ""
+        })
+
+        await post.save();
+
+        return res.status(200).json({ message: "Post Created"})
+    
+    } catch (error) {
+        return res.status(500).json({ message: error.message })
+    }
 }
